ce8838ef380dd0363d632f7a704a70dbe9f1ca2f
ZyroBad <zyrobadj@gmail.com>
2026-05-17 19:28:55 -0600

    frontend vite + estilos base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div className="app">
+      <header className="encabezado">
+        <h1>Mi Colección Personal</h1>
+        <p className="sub">
+          Bitácora rápida de sesiones (todavía bien fase 1, tranqui).
+        </p>
+      </header>
+
+      <main className="contenido">
+        <div className="bloque">
+          <h2>Sesiones</h2>
+          <p>Acá vamos a crear el CRUD en la siguiente parte.</p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
